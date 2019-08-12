f0242938b5183afcc530c48830c7cc3b3062ea41
volodymyr.bobrov <volodymyr.bobrov@globallogic.com>
2019-08-12 17:10:03 +0300

    #task1 (p1 - p2).

---
+++ src/homeTasks/Task1.java
@@ -0,0 +1,29 @@
+package homeTasks;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        //1. Say Hi to user after entering his name via console
+/*        System.out.println("Please enter your name: ");
+        String s = new Scanner(System.in).nextLine();
+        System.out.println("Hi " + s);*/
+
+        //2. Get data from console, reverse it and return
+        /*System.out.println("Please enter several symbols: ");
+        String v = new Scanner(System.in).nextLine();
+        char[] arrayA = v.toCharArray();
+        char[] reversed = new char[arrayA.length];
+        int j = 0;
+        for (int i = arrayA.length; i > 0; i--) {
+            reversed[j] = arrayA[i-1];
+            j++;
+        }
+        System.out.println(reversed);*/
+
+        //3.
+
+
+    }
+}
