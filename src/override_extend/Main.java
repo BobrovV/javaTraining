0c675183546bfea8e003b48022dfba11795a8e7e
volodymyr.bobrov <volodymyr.bobrov@globallogic.com>
2019-08-29 13:58:13 +0300

    Aug29.1

---
+++ src/override_extend/Main.java
@@ -0,0 +1,10 @@
+package override_extend;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        a.m();
+        A b = new B();
+        b.m();
+    }
+}
